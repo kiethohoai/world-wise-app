59c4629fe20be8ec63a1492cb9e4ff10e133bb0e
kiethohoai <kietho.060193@gmail.com>
2024-11-25 14:29:34 +0700

    005 Linking Between Routes With Link  and NavLink

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise App</h1>
+
+      <Link to="/product">Product</Link>
+    </div>
+  );
 }
 
 export default Homepage;
