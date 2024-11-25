7a0d2aeb1b7525d50388e2a22ca6878b5e52bd21
kiethohoai <kietho.060193@gmail.com>
2024-11-25 21:28:15 +0700

    010 Nested Routes and Index Route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,16 @@
+import { Outlet } from 'react-router-dom';
 import AppNav from './AppNav';
 import Logo from './Logo';
 import style from './Sidebar.module.css';
+import Footer from './Footer';
 
 function Sidebar() {
   return (
     <div className={style.sidebar}>
       <Logo />
       <AppNav />
-
-      <p>List of cities</p>
-
-      <footer className={style.footer}>
-        <p className={style.copyright}>
-          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-        </p>
-      </footer>
+      <Outlet />
+      <Footer />
     </div>
   );
 }
